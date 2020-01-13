a218e07cbc0f2cc028cda15e29c8f06315f12434
mrizkimaulidan <localmrm@gmail.com>
2020-01-13 08:26:59 -0500

    Membuat auth api login

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function login()
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = Auth::user();
+            $success['id'] = $user->id;
+            $success['name'] = $user->name;
+            $success['token'] = $user->createToken('Passport Token')->accessToken;
+            return response()->json(['message' => 200, 'results' => $success]);
+        }
+    }
+}
