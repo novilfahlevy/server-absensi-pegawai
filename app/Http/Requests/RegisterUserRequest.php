8a1a53ad85836ef154f6702883b96fb94df93c40
Najib <muhariri.najibullah@gmail.com>
2020-01-13 20:45:30 +0800

    create user(not included photo profile) and get all user)

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use ApiResponse;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:100',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'profile_imgpath' => 'image|mimes:jpeg,png,gif,svg|max:1024',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => 'Nama tidak boleh kosong',
+            'name.max' => 'karakter sudah melebihi batas maksimal',
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Format email yang anda masukan salah',
+            'email.unique' => 'Email ini telah ada sebelumnya',
+            'password.required' => 'Password tidak boleh kosong',
+            'profile_imgpath.image' => 'File yang harus dimasukkan harus gambar',
+            'profile_imgpath.mimes' => 'Extensi gambar yang anda masukan tidak dapat digunakan',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json($validator->errors(),422)
+        );
+    }
+}
