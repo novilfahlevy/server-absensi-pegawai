d9945eab1957a12b7d72d53695ccc9e062fef19e
MirrorBottle <setiawanbayu66152@gmail.com>
2020-01-14 09:55:57 +0800

    Changing laravel-cors to fruitcake

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::post('auth/register', 'Api\UserController@register');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/user', 'UserController@index');
+Route::get('/user', 'Api\UserController@index');
 
-Route::post('/user/store', 'UserController@store');
+Route::post('/user/store', 'Api\UserController@store');
