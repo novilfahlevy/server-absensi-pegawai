22fc61069b12d05217e264be61f57d0ccb23441d
mrizkimaulidan <localmrm@gmail.com>
2020-01-13 20:21:47 -0500

    refactoring

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,9 +5,21 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterUserRequest;
+use App\User;
+use App\Role;
+use ApiResponse;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+
+        $user = User::all();
+
+        return response()->json(['status' => '200', 'message' => 'Sukses', 'user' => $user]);
+    }
+
     public function login()
     {
         if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
@@ -17,5 +29,17 @@ class UserController extends Controller
             $success['token'] = $user->createToken('Passport Token')->accessToken;
             return response()->json(['status' => 200, 'message' => $success]);
         }
+    }
+
+    public function store(RegisterUserRequest $request)
+    {
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $role = Role::find(2);
+        $user->assignRole($role);
+
+        return response()->json(['status' => '200', 'message' => 'Sukses', 'user' => $user]);
     }
 }
