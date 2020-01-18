9b6727a5afbfad602acd6e42da007039f07f7d0f
mrizkimaulidan <localmrm@gmail.com>
2020-01-17 23:52:30 -0500

    remove unused namespace

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,11 +11,8 @@ use App\Http\Requests\RegisterUserRequest;
 use Intervention\Image\Facades\Image;
 use App\User;
 use App\Role;
-use File;
-use App\Helpers\ApiResponse;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Passport\Passport;
-use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -93,47 +90,35 @@ class UserController extends Controller
 
     public function editProfile(Request $request)
     {
-        $request->validate([
-            'profile' => 'required|image|mimes:jpeg,png,svg|max:2048',
-        ],
-        [
-            'profile.required' => 'Masukkan gambar terlebih dahulu',
-            'profile.image' => 'File yang harus dimasukkan harus gambar',
-            'profile.mimes' => 'Extensi gambar yang anda masukan tidak dapat digunakan',
-            'profile.max' => 'Profile anda sudah melebihi batas ukuran'
-        ]);
+        $request->validate(
+            [
+                'profile' => 'required|image|mimes:jpeg,png,svg|max:2048',
+            ],
+            [
+                'profile.required' => 'Masukkan gambar terlebih dahulu',
+                'profile.image' => 'File yang harus dimasukkan harus gambar',
+                'profile.mimes' => 'Extensi gambar yang anda masukan tidak dapat digunakan',
+                'profile.max' => 'Profile anda sudah melebihi batas ukuran'
+            ]
+        );
 
-            if ($request->hasFile('profile')) {
-                //Get filename with the extention
-                $fileNameWithExtention = $request->file('profile')->getClientOriginalName();
-                //get just filename
-                $fileName = pathinfo($fileNameWithExtention, PATHINFO_FILENAME);
-                //Get just extention
-                $extention = $request->file('profile')->getClientOriginalExtension();
-                //Filename to store
-                $filenameToStore = $fileName.'_'.time().'.'.$extention;
-                //saving Image
-                $user = User::find($request->user_id);
+        if ($request->hasFile('profile')) {
+            //Get filename with the extention
+            $fileNameWithExtention = $request->file('profile')->getClientOriginalName();
+            //get just filename
+            $fileName = pathinfo($fileNameWithExtention, PATHINFO_FILENAME);
+            //Get just extention
+            $extention = $request->file('profile')->getClientOriginalExtension();
+            //Filename to store
+            $filenameToStore = $fileName . '_' . time() . '.' . $extention;
+            //saving Image
+            $user = User::find($request->user_id);
 
-                if($user->profile !== 'default.jpg'){
-                    Storage::delete('public/profiles/'.$user->profile);
-                }
-                $profileimagepath = public_path().'/storage/profiles/';
-                $profileimageUrl = '/storage/profiles/'.$filenameToStore;
-                $profileimage = Image::make($request->file('profile'));
-                $canvas = Image::canvas(300,300);
-                $profileimage->resize(300,300, function ($constrait){
-                    $constrait->aspectRatio();
-                });
-                $canvas->insert($profileimage, 'center');
-                $canvas->save($profileimagepath.$filenameToStore);
-                //Updating user
-                $user->profile = $filenameToStore;
-                $user->save();
-
-                return response()->json(['status' => 200, 'message' => 'Profil anda telah di update' , 'data' => url($profileimageUrl)]);
+            if ($user->profile !== 'default.jpg') {
+                Storage::delete('public/profiles/' . $user->profile);
             }
             $profileimagepath = public_path() . '/storage/profiles/';
+            $profileimageUrl = '/storage/profiles/' . $filenameToStore;
             $profileimage = Image::make($request->file('profile'));
             $canvas = Image::canvas(300, 300);
             $profileimage->resize(300, 300, function ($constrait) {
@@ -145,15 +130,29 @@ class UserController extends Controller
             $user->profile = $filenameToStore;
             $user->save();
 
-            return response()->json(['status' => 200, 'message' => 'Profil anda telah di update', 'data' => url($profileimagepath)]);
+            return response()->json(['status' => 200, 'message' => 'Profil anda telah di update', 'data' => url($profileimageUrl)]);
+        }
+        $profileimagepath = public_path() . '/storage/profiles/';
+        $profileimage = Image::make($request->file('profile'));
+        $canvas = Image::canvas(300, 300);
+        $profileimage->resize(300, 300, function ($constrait) {
+            $constrait->aspectRatio();
+        });
+        $canvas->insert($profileimage, 'center');
+        $canvas->save($profileimagepath . $filenameToStore);
+        //Updating user
+        $user->profile = $filenameToStore;
+        $user->save();
+
+        return response()->json(['status' => 200, 'message' => 'Profil anda telah di update', 'data' => url($profileimagepath)]);
     }
 
     public function cari($name)
     {
 
-        $user = User::where('name', 'LIKE', '%'. $name . '%')->get();
-        if(!$user->isEmpty()){
-            return response()->json(['code' => 200, 'message' => 'berhasil mencari data' , 'data' => $user]);
+        $user = User::where('name', 'LIKE', '%' . $name . '%')->get();
+        if (!$user->isEmpty()) {
+            return response()->json(['code' => 200, 'message' => 'berhasil mencari data', 'data' => $user]);
         }
         return response()->json(['code' => 400, 'message' => 'Kata yang anda cari tidak ditemukan']);
     }
