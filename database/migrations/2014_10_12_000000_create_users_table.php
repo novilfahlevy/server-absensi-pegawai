76402eed34e5766aa5d1b552c7b0730e0cd2f650
mrizkimaulidan <localmrm@gmail.com>
2020-01-21 08:10:04 -0500

    added username, nomor_handphone and alamat on users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
+            $table->string('nomor_handphone')->nullable();
+            $table->string('alamat')->nullable();
             $table->string('profile_imgpath')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
