9a2ea0beb122defa79c7e164cc8fe40c33252141
mrizkimaulidan <localmrm@gmail.com>
2020-01-21 01:46:37 -0500

    create AbsensiSeeder data

--- database/seeds/AbsensiSeeder.php
+++ database/seeds/AbsensiSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Absensi;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class AbsensiSeeder extends Seeder
 {
@@ -11,6 +13,18 @@ class AbsensiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $carbon = new Carbon();
+
+        Absensi::create([
+            'user_id' => 2,
+            'tanggal' => $carbon->toDateString(),
+            'absensi_masuk' => $carbon->toTimeString(),
+            'absensi_keluar' => $carbon->toTimeString(),
+            'keterangan' => 'Absensi',
+            'foto_absensi_masuk' => 'masuk.jpg',
+            'foto_absensi_keluar' => 'keluar.jpg',
+            'latitude' => '1.111',
+            'longitude' => '1.111',
+        ]);
     }
 }
