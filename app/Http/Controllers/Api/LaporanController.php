1e9cf843917b9172023d48f73098185953f75594
mrizkimaulidan <localmrm@gmail.com>
2020-01-21 21:08:06 -0500

    add more response

--- app/Http/Controllers/Api/LaporanController.php
+++ app/Http/Controllers/Api/LaporanController.php
@@ -17,28 +17,44 @@ class LaporanController extends Controller
                     'minggu2' => 20,
                     'minggu3' => 30,
                     'minggu4' => 40,
-                    'total' => 10 + 20 + 30 + 40
+                    'performance' => [
+                        'total_jam_kerja' => 10 + 20 + 30 + 40,
+                        'total_terlambat' => 20,
+                        'total_lembur' => 40,
+                    ]
                 ],
                 'Ujay' => [
                     'minggu1' => 50,
                     'minggu2' => 60,
                     'minggu3' => 70,
                     'minggu4' => 80,
-                    'total' => 50 + 60 + 70 + 80
+                    'performance' => [
+                        'total_jam_kerja' => 50 + 60 + 70 + 80,
+                        'total_terlambat' => 10,
+                        'total_lembur' => 6,
+                    ]
                 ],
                 'Bagus' => [
                     'minggu1' => 90,
                     'minggu2' => 100,
                     'minggu3' => 110,
                     'minggu4' => 120,
-                    'total' => 90 + 100 + 110 + 120
+                    'performance' => [
+                        'total_jam_kerja' => 90 + 100 + 110 + 120,
+                        'total_terlambat' => 9,
+                        'total_lembur' => 9,
+                    ]
                 ],
                 'Kinay' => [
                     'minggu1' => 130,
                     'minggu2' => 140,
                     'minggu3' => 150,
                     'minggu4' => 160,
-                    'total' => 130 + 140 + 150 + 160
+                    'performance' => [
+                        'total_jam_kerja' => 130 + 140 + 150 + 160,
+                        'total_terlambat' => 2,
+                        'total_lembur' => 7,
+                    ]
                 ]
             ],
             'total_jam_per_bulan' => [100, 123, 124, 128],
