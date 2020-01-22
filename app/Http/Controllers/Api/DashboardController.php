2604172ad1404dfe34bf1ebae97cdec238bd9694
novilfahlevy <novilfreon@gmail.com>
2020-01-22 15:26:40 +0800

    membuat data dashboard

---
+++ app/Http/Controllers/Api/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\User;
+use App\Absensi;
+
+class DashboardController extends Controller
+{
+    public function index() {
+        $total_pegawai = User::get()->count();
+        $total_pegawai_absen = Absensi::get()->count();
+        return response()->json([
+            'status' => 200,
+            'data' => [
+                'total_pegawai' => $total_pegawai,
+                'total_pegawai_absen' => $total_pegawai_absen,
+                'total_pegawai_belum_absen' => $total_pegawai - $total_pegawai_absen,
+                'total_pegawai_lembur' => 'Belum dibuat model lembur nya'
+            ]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ Route::group(['middleware' => ['auth:api']], function () {
         Route::get('/absensi', 'Api\AbsensiController@index');
         // Route::get('/absensi/{keterangan}', 'Api\AbsensiController@cari');
         Route::get('/absensi/laporan', 'Api\LaporanController@index');
+        Route::get('/dashboard', 'Api\DashboardController@index');
     });
 
     Route::group(['middleware' => ['role:Admin|User']], function () {
