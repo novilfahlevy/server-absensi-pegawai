3efd2cc3ac62b3a55245001a910d1e0f6939b0ae
mrizkimaulidan <localmrm@gmail.com>
2020-01-23 01:08:49 -0500

    create lemburs migration

---
+++ database/migrations/2020_01_23_140530_create_lemburs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLembursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lemburs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('absensi_id');
+            $table->time('lembur_awal');
+            $table->time('lembur_akhir');
+            $table->integer('konsumsi');
+            $table->string('foto');
+            $table->boolean('status');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('absensi_id')->references('id')->on('absensis')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lemburs');
+    }
+}
