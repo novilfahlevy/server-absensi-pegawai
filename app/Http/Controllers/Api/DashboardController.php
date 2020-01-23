16c3fe1d32a72dcb7f7f3286b558b116bfad7b9b
novilfahlevy <novilfreon@gmail.com>
2020-01-23 09:37:53 +0800

    menambah data user pada data absensi

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -13,6 +13,12 @@ class DashboardController extends Controller
     public function index() {
         $total_pegawai = User::get()->count();
         $total_pegawai_absen = Absensi::get()->count();
+
+        $absensi = Absensi::all()->sortBy('absensi_masuk')->take(5);
+        foreach ( $absensi as $key => $absen ) {
+            $absensi[$key]['name'] = $absen->user->name;
+        }
+
         return response()->json([
             'status' => 200,
             'data' => [
@@ -20,7 +26,7 @@ class DashboardController extends Controller
                 'total_pegawai_absen' => $total_pegawai_absen,
                 'total_pegawai_belum_absen' => $total_pegawai - $total_pegawai_absen,
                 'total_pegawai_lembur' => 'Belum dibuat model lembur nya',
-                'pegawai_sudah_absen' => Absensi::all()->sortBy('absensi_masuk')->take(5),
+                'pegawai_sudah_absen' => $absensi,
                 'pegawai_minta_lembur' => 'Belum dibuat model lembur nya'
             ]
         ]);
