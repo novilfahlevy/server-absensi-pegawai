3b5bf3ee520608cebdcd0fff02dcf49324705f21
novilfahlevy <novilfreon@gmail.com>
2020-01-23 09:55:21 +0800

    mengirim data pegawai belum absen dan mengurangi data yang dikirim

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -14,9 +14,21 @@ class DashboardController extends Controller
         $total_pegawai = User::get()->count();
         $total_pegawai_absen = Absensi::get()->count();
 
-        $absensi = Absensi::all()->sortBy('absensi_masuk')->take(5);
-        foreach ( $absensi as $key => $absen ) {
-            $absensi[$key]['name'] = $absen->user->name;
+        $sudah_absensi = [];
+        foreach ( Absensi::all()->sortBy('absensi_masuk')->take(5) as $absen ) {
+            $sudah_absensi[] = [
+                'name' => $absen->user->name, 
+                'absensi_masuk' => $absen->absensi_masuk,
+                'absensi_keluar' => $absen->absensi_keluar,
+                'id' => $absen->user->id
+            ];
+        }
+
+        $belum_absensi = [];
+        foreach ( User::all() as $user ) {
+            if ( !Absensi::find($user->id) ) {
+                $belum_absensi[] = ['name' => User::find($user->id)->name, 'id' => $user->id];
+            }
         }
 
         return response()->json([
@@ -26,8 +38,8 @@ class DashboardController extends Controller
                 'total_pegawai_absen' => $total_pegawai_absen,
                 'total_pegawai_belum_absen' => $total_pegawai - $total_pegawai_absen,
                 'total_pegawai_lembur' => 'Belum dibuat model lembur nya',
-                'pegawai_sudah_absen' => $absensi,
-                'pegawai_minta_lembur' => 'Belum dibuat model lembur nya'
+                'pegawai_sudah_absen' => $sudah_absensi,
+                'pegawai_belum_absen' => collect($belum_absensi)->take(5)
             ]
         ]);
     }
