dad0a05e124b4fc725fc7f57b8836af25aa6d2cf
mrizkimaulidan <localmrm@gmail.com>
2020-01-26 22:50:01 -0500

    unworking

--- app/Http/Controllers/Api/LemburController.php
+++ app/Http/Controllers/Api/LemburController.php
@@ -170,12 +170,12 @@ class LemburController extends Controller
     // {
     //     $lembur = Lembur::all();
     //     dd($lembur);
-    //     // // $lembur = Lembur::where('tanggal', 'LIKE', '%' . $keyword . '%')->get();
+    //     $lembur = Lembur::where('tanggal', 'LIKE', '%' . $keyword . '%')->get();
 
-    //     // if (!$lembur->isEmpty()) {
-    //     //     return response()->json(['code' => 200, 'message' => 'Berhasil mencari data!', 'data' => $lembur]);
-    //     // }
+    //     if (!$lembur->isEmpty()) {
+    //         return response()->json(['code' => 200, 'message' => 'Berhasil mencari data!', 'data' => $lembur]);
+    //     }
 
-    //     // return response()->json(['code' => 400, 'message' => 'Kata yang anda cari tidak ditemukan!']);
+    //     return response()->json(['code' => 400, 'message' => 'Kata yang anda cari tidak ditemukan!']);
     // }
 }
