53a113e3ecc6b86a5a2988afdadb99d3abcf2560
mrizkimaulidan <rizmaul@yandex.com>
2020-03-09 22:22:12 -0400

    add izin model

--- app/Http/Controllers/Api/Android/AbsensiController.php
+++ app/Http/Controllers/Api/Android/AbsensiController.php
@@ -16,6 +16,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Date;
+use App\Izin;
 
 class AbsensiController extends Controller
 {
